7931c4465aad4ce06dd52eb33d6d8011feba8f19
Aman Singh <aman.iit96@gmail.com>
2023-08-20 22:16:41 +0530

    headers modified

--- dist/index.js
+++ dist/index.js
@@ -14,9 +14,9 @@ try {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(function (req, res, next) {
-        res.append('Access-Control-Allow-Origin', ['*']);
-        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        res.append('Access-Control-Allow-Origin', '*');
+        res.append('Access-Control-Allow-Methods', '*');
+        res.append('Access-Control-Allow-Headers', '*');
         next();
     });
     app.use('/aws', new aws_1.AwsRouter().router);
--- lib/index.ts
+++ lib/index.ts
@@ -18,9 +18,9 @@ try {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Allow-Origin', '*');
+    res.append('Access-Control-Allow-Methods', '*');
+    res.append('Access-Control-Allow-Headers', '*');
     next();
   });
 
